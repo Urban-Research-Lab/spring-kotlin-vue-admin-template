5ab100e221b5b182d8cb7150b9076e5062dec1f7
jedi-philosopher <smirnov.egor.v@gmail.com>
2020-07-23 17:34:33 +0300

    SBadmin 2 added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,16 +6,32 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Dashboard from "@/pages/Dashboard";
 import SignIn from "@/pages/SignIn";
+import Landing from "@/pages/Landing";
+import LoadScript from 'vue-plugin-load-script';
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
+Vue.use(LoadScript);
+
+Vue.loadScript("/sbadmin/vendor/jquery/jquery.min.js")
+Vue.loadScript("/sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js")
+global.jQuery = require('jquery');
+global.$ = global.jQuery;
+window.$ = global.jQuery;
+window.jQuery = global.jQuery;
+
+Vue.loadScript("/sbadmin/js/sb-admin-2.min.js")
+
+import("../public/sbadmin/css/sb-admin-2.css")
+import("../public/sbadmin/vendor/fontawesome-free/css/all.min.css")
 
 const router = new VueRouter({
   routes:  [
     { path: '/', component: Dashboard },
-    { path: '/signin', component: SignIn}
+    { path: '/signin', component: SignIn},
+    { path: '/landing', component: Landing}
   ]
 });
 
--- frontend/src/pages/SignIn.vue
+++ frontend/src/pages/SignIn.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <h1>Sign in</h1>
 </template>
 
 <script>
