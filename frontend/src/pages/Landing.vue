20b68b28a952d5d111817ebe45ed18551d9df18a
esmirnov_ifree <esmirnov>
2020-07-28 12:29:27 +0300

    Nicer landing page

--- frontend/src/pages/Landing.vue
+++ frontend/src/pages/Landing.vue
@@ -1,5 +1,38 @@
 <template>
-<h1>This is landing page</h1>
+    <div class="container">
+        <b-row class="mb-5">
+            <b-col>
+                <h1 class="text-center">This is landing page</h1>
+            </b-col>
+        </b-row>
+        <b-row class="mb-5">
+            <b-col>
+                <h3>Available to everyone</h3>
+                <p>Does not need logging in</p>
+            </b-col>
+            <b-col>
+                <h3>
+                    Easy to lay out
+                </h3>
+                <p>
+                    With bootstrap-vue support
+                </p>
+            </b-col>
+            <b-col>
+                <h3>
+                    More features coming soon
+                </h3>
+                <p>
+                    Lorem ipsum
+                </p>
+            </b-col>
+        </b-row>
+        <b-row class="mb-5">
+            <b-col>
+               <b-button variant="success" href="/" style="width:100%">Start</b-button>
+            </b-col>
+        </b-row>
+    </div>
 </template>
 
 <script>
