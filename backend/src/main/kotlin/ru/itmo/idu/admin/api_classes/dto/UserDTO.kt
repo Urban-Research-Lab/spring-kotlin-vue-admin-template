cbbbb3343fbd789bb03e7093513e00e86460f2a6
Egor Smirnov <smirnov.egor.v@gmail.com>
2020-12-26 21:50:15 +0300

    User statuses work now

--- backend/src/main/kotlin/ru/itmo/idu/admin/api_classes/dto/UserDTO.kt
+++ backend/src/main/kotlin/ru/itmo/idu/admin/api_classes/dto/UserDTO.kt
@@ -1,6 +1,7 @@
 package ru.itmo.idu.admin.api_classes.dto
 
 import ru.itmo.idu.admin.model.User
+import ru.itmo.idu.admin.model.UserStatus
 
 data class UserDTO(
         val id: Long,
@@ -8,7 +9,8 @@ data class UserDTO(
         val name: String?,
         val authorities: List<String>,
         val roles: List<RoleDTO>,
-        val registrationTimestamp: Long
+        val registrationTimestamp: Long,
+        val status: String
 ) {
     companion object {
         fun fromUser(user: User): UserDTO {
@@ -18,8 +20,13 @@ data class UserDTO(
                     user.displayName,
                     user.roles.flatMap { it.permissions }.map { it.name },
                     user.roles.map { RoleDTO.fromRoleSimplified(it) },
-                    user.registrationTimestamp
+                    user.registrationTimestamp,
+                    user.status.name
             )
+        }
+
+        fun fromUserSimple(user: User): UserDTO {
+            return UserDTO(user.id, user.email, user.displayName, listOf(), listOf(), user.registrationTimestamp, user.status.name)
         }
     }
 }
--- frontend/src/pages/users/UserList.vue
+++ frontend/src/pages/users/UserList.vue
@@ -1,87 +1,139 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-    <div>
-        <b-row>
-            <b-col>
-                <h1>{{ $t("user.list")}}</h1>
-            </b-col>
-            <b-col class="flex-row-reverse">
-                <b-button variant="success" to="/create_user"><i class="fa fa-user-plus"></i>{{ $t("user.create")}}</b-button>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <b-table id="users-table" striped hover
-                         :items="itemProvider"
-                         :per-page="15"
-                         :fields="fields"
-                         ref="table"
-                >
-                    <template v-slot:cell(registrationTimestamp)="data">
-                        {{ new Date(data.value).toLocaleDateString() }}
-                    </template>
-                    <template v-slot:cell(roles)="data">
-                        {{ data.value.map(role => role.name) }}
-                    </template>
+  <div>
+    <b-row>
+      <b-col>
+        <h1>{{ $t("user.list")}}</h1>
+      </b-col>
+      <b-col class="flex-row-reverse">
+        <b-button variant="success" to="/create_user"><i class="fa fa-user-plus"></i>{{ $t("user.create")}}</b-button>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-table id="users-table" striped hover
+                 :items="itemProvider"
+                 :per-page="15"
+                 :fields="fields"
+                 ref="table"
+        >
+          <template v-slot:cell(registrationTimestamp)="data">
+            {{ new Date(data.value).toLocaleDateString() }}
+          </template>
+          <template v-slot:cell(roles)="data">
+            {{ data.value.map(role => role.name) }}
+          </template>
 
-                    <template v-slot:cell(actions)="row">
-                        <b-button size="sm" @click="editUserClicked(row.item)" class="btn-primary mr-1">
-                            <i class="fa fa-edit"></i> {{ $t("edit")}}
-                        </b-button>
-                        <b-button size="sm" @click="deleteUserClicked(row.item)" class="btn-danger">
-                            <i class="fa fa-trash"></i> {{ $t("delete")}}
-                        </b-button>
-                    </template>
+          <template v-slot:cell(actions)="row">
+            <b-button size="sm" @click="editUserClicked(row.item)" class="btn-primary mr-1">
+              <i class="fa fa-edit"></i> {{ $t("edit")}}
+            </b-button>
+            <b-button size="sm" @click="activateUserClicked(row.item)" class="btn-info mr-1" v-if="row.item.status === 'REGISTERED'">
+              <i class="fa fa-check"></i> {{ $t("user.activate")}}
+            </b-button>
+            <span v-if="hasAuthority('BAN_USERS')">
+                            <b-button size="sm" @click="banUserClicked(row.item)" class="btn-warning mr-1" v-if="row.item.status !== 'BANNED'">
+                                <i class="fa fa-stop-circle"></i> {{ $t("user.ban")}}
+                            </b-button>
+                            <b-button size="sm" @click="unbanUserClicked(row.item)" class="btn-success mr-1" v-if="row.item.status === 'BANNED'">
+                                <i class="fa fa-play-circle"></i> {{ $t("user.unban")}}
+                            </b-button>
+                        </span>
+            <b-button size="sm" @click="deleteUserClicked(row.item)" class="btn-danger">
+              <i class="fa fa-trash"></i> {{ $t("delete")}}
+            </b-button>
+          </template>
 
-                </b-table>
-                <b-pagination
-                        v-model="currentPage"
-                        :total-rows="rows"
-                        :per-page="15"
-                        aria-controls="users-table"
-                ></b-pagination>
-            </b-col>
-        </b-row>
+        </b-table>
+        <b-pagination
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="15"
+            aria-controls="users-table"
+        ></b-pagination>
+      </b-col>
+    </b-row>
 
-    </div>
+    <b-modal id="modal-1" :title="$t('user.ban')" v-model="showBanModal" @ok="doBanUser" :ok-disabled="!reasonEntered">
+      Please enter reason for banning {{userToBan.email}}
+      <b-form-textarea v-model="banReason" class="mt-3">
+
+      </b-form-textarea>
+    </b-modal>
+
+  </div>
 </template>
 
 <script>
-    import UserAPI from "../../mixins/UserAPI";
+import UserAPI from "../../mixins/UserAPI";
 
-    export default {
-        name: "UserList",
-        data() {
-            return {
-                currentPage: 1,
-                rows: 0,
-                fields: ["id", "email", "name", "roles", "registrationTimestamp", "actions"]
-            }
-        },
-        mixins: [UserAPI],
-        methods: {
-            async itemProvider(ctx) {
-                return this.listUsers(ctx.currentPage - 1, ctx.perPage)
-            },
-            // called by parent component
-            async updateData() {
-                this.currentPage = await this.countUsers()
-                this.$refs.table.refresh();
-            },
-            editUserClicked(item) {
-                this.$router.push("/users/" + item.id)
-            },
-
-            async deleteUserClicked(item) {
-                if (confirm("Do you really want to delete this item?")) {
-                    await this.deleteUser(item.id);
-                    this.updateData();
-                }
-            }
-        },
-        async mounted() {
-           this.updateData()
-        }
+export default {
+  name: "UserList",
+  data() {
+    return {
+      currentPage: 1,
+      rows: 0,
+      fields: ["id", "email", "name", "roles", "registrationTimestamp", "actions"],
+      showBanModal: false,
+      userToBan: {},
+      banReason: ""
     }
+  },
+  mixins: [UserAPI],
+  methods: {
+    async itemProvider(ctx) {
+      return this.listUsers(ctx.currentPage - 1, ctx.perPage)
+    },
+    // called by parent component
+    async updateData() {
+      this.currentPage = await this.countUsers()
+      this.$refs.table.refresh();
+    },
+    editUserClicked(item) {
+      this.$router.push("/users/" + item.id)
+    },
+
+    async deleteUserClicked(item) {
+      if (confirm("Do you really want to delete this item?")) {
+        await this.deleteUser(item.id);
+        this.updateData();
+      }
+    },
+    banUserClicked(item) {
+      this.userToBan = item;
+      this.showBanModal = true;
+    },
+    async activateUserClicked(item) {
+      var activated = await this.activateUserAdmin(item.email);
+      if (activated) {
+        this.successToast("User activated");
+        this.updateData();
+      } else {
+        this.errorToast("User cannot be activated");
+      }
+    },
+    async doBanUser() {
+      await this.banUser(this.userToBan.id, this.banReason);
+      this.updateData();
+    },
+    async unbanUserClicked(item) {
+      if (confirm("Do you really want to unban this user?")) {
+        await this.unbanUser(item.id);
+        this.updateData();
+      }
+    },
+    hasAuthority(name) {
+      return this.$store.getters.hasAuthority(name)
+    }
+  },
+  computed: {
+    reasonEntered() {
+      return this.banReason.length > 0
+    }
+  },
+  async mounted() {
+    this.updateData()
+  }
+}
 </script>
 
 <style scoped>
