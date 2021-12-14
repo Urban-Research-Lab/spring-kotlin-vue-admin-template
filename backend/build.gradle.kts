2ee48d78a008cab06f61ad855f1ccda8cc63e91d
Egor Smirnov <smirnov.egor.v@gmail.com>
2021-12-14 17:06:18 +0300

    Updated dependencies and gradle plugins

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -6,24 +6,24 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath(kotlin("gradle-plugin", version= "1.4.32"))
-        classpath(kotlin("noarg", version= "1.4.32"))
+        classpath(kotlin("gradle-plugin", version= "1.6.10"))
+        classpath(kotlin("noarg", version= "1.6.10"))
     }
 }
 
 
 plugins {
-    id("org.springframework.boot") version "2.4.4"
+    id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
-    kotlin("plugin.noarg") version "1.4.32"
-    kotlin("plugin.jpa") version "1.4.32"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.noarg") version "1.6.10"
+    kotlin("plugin.jpa") version "1.6.10"
 }
 
 
 springBoot {
-    mainClassName = "ru.itmo.idu.admin.Prod"
+    mainClass.set("ru.itmo.idu.admin.Prod")
 }
 
 group = "ru.itmo.idu.admin-template"
@@ -39,7 +39,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.security:spring-security-oauth2-client")
-    implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.2.6.RELEASE")
+    implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.6.1")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -49,18 +49,18 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
-    implementation("jakarta.validation:jakarta.validation-api:2.0.2")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.0")
 
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("com.google.guava:guava:28.2-jre")
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.7")
+    implementation("com.google.guava:guava:31.0.1-jre")
     implementation("commons-io:commons-io:2.6")
-    implementation("org.apache.commons:commons-lang3:3.11")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.apache.commons:commons-collections4:4.2")
-    implementation("mysql:mysql-connector-java:8.0.23")
+    implementation("mysql:mysql-connector-java:8.0.25")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
 
@@ -70,8 +70,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.session:spring-session-core")
 
-    testImplementation("junit:junit:4.13")
-    testImplementation("com.h2database:h2:1.4.197")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.h2database:h2:2.0.202")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
