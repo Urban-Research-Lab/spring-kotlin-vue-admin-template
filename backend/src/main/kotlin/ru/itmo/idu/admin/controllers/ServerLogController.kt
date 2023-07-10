abd4e9de4b55da3ea3ca4cddd05beeb130600f0c
Aleksandr <alexander.savichev@gmail.com>
2023-07-10 22:42:56 +0300

    Подключить Springdoc Openapi - добавлены описания методов для API и основные коды ответов. Добавлен MailConfiguration класс для устранения ошибки о невозможности создать Bean JavaMailSender

--- backend/src/main/kotlin/ru/itmo/idu/admin/controllers/ServerLogController.kt
+++ backend/src/main/kotlin/ru/itmo/idu/admin/controllers/ServerLogController.kt
@@ -1,10 +1,16 @@
 package ru.itmo.idu.admin.controllers
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.itmo.idu.admin.api_classes.ObjectListResponse
+import ru.itmo.idu.admin.api_classes.dto.LogEntryDTO
 import ru.itmo.idu.admin.services.LogService
 
 @RestController
@@ -15,6 +21,16 @@ class ServerLogController(
 ) {
 
     @GetMapping(path = ["", "/"])
+    @Operation(summary = "Get logs about events",
+        description = "Get logs about events")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Logs were successfully loaded", content = [
+            (Content(mediaType = "application/json", schema = Schema(implementation = LogEntryDTO::class)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun getLogs(): ObjectListResponse {
         return ObjectListResponse(logService.getLogEntries())
     }
--- backend/src/main/kotlin/ru/itmo/idu/admin/controllers/UserController.kt
+++ backend/src/main/kotlin/ru/itmo/idu/admin/controllers/UserController.kt
@@ -1,5 +1,11 @@
 package ru.itmo.idu.admin.controllers
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
@@ -22,16 +28,38 @@ class UserController(
 ) {
 
     @PostMapping("new")
+    @Operation(summary = "Add a new user",
+        description = "Add a new user to include UserDTO parameters into a database")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "User was created", content = [
+            (Content(mediaType = "application/json", schema = Schema(implementation = UserDTO::class)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun registerUser(
             @RequestBody
             request: UserRegistrationRequest
     ): ObjectResponse<UserDTO> {
         log.info("registerUser(request = {})", request)
-        val user = userService.registerUser(request);
+        val user = userService.registerUser(request)
         return ObjectResponse(UserDTO.fromUser(user))
     }
 
     @PostMapping("activate")
+    @Operation(summary = "Activate user",
+        description = "Activate user by the given token and searching matches in database")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "User was activated", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "User wasn't found", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun activateUser(
             @RequestParam token: String
     ): StringResponse {
@@ -42,6 +70,18 @@ class UserController(
 
     @PreAuthorize("hasAuthority('MANAGE_USERS')")
     @PostMapping("/admin/activate")
+    @Operation(summary = "Activate admin",
+        description = "Activate admin by the given email with checking if the user has been activated and with searching matches in database")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Admin was activated", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "There is no admin account with such Email", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun activateUserAdmin(@RequestParam email: String): BaseResponse {
         log.info("activateUserAdmin(email = {})", email)
         userService.activateUserAdmin(email)
@@ -50,6 +90,20 @@ class UserController(
 
     @PreAuthorize("hasAuthority('BAN_USERS')")
     @PostMapping("/{id}/ban")
+    @Operation(summary = "Ban user",
+        description = "Ban user with a stated Id and reason. While doing so, there is a check if a current user has an authority to ban other members, user with this Id hadn't already been banned or current user is trying to ban itself")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "User has been banned", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "401", description = "Not enough privileges to ban users", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "There is no user with such Id", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun banUser(
             @PathVariable("id") id: Long,
             @RequestParam reason: String
@@ -61,16 +115,42 @@ class UserController(
 
     @PreAuthorize("hasAuthority('BAN_USERS')")
     @PostMapping("/{id}/unban")
+    @Operation(summary = "Cancel ban status of the user",
+        description = "Cancel ban status of the user with a stated Id and reason. While doing so, there is a check if a current user has an authority to unban other members, user with this Id hadn't already been unbanned or current user is trying to unban itself")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Ban status of the current user has been lifted", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "401", description = "Not enough privileges to unban users", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "There is no user with such Id", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun unbanUser(
             @PathVariable("id") id: Long
     ): BaseResponse {
-        log.info("unbanUser(id = {}, reason = {})", id)
+        log.info("unbanUser(id = {})", id)
         userService.unbanUser(id)
         return BaseResponse(ResponseCodes.OK)
     }
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/{id}")
+    @Operation(summary = "Update user",
+        description = "Update information of the user with a stated Id. While doing so, there is a check if a current user has an authority to update info of other members and all input values are correct")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "User info was updated", content = [
+            (Content(mediaType = "application/json", schema = Schema(implementation = UserDTO::class)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "There is no user with such Id", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun updateUser(
             @PathVariable("id") id: Long,
             @RequestBody request: UserUpdateRequest
@@ -82,6 +162,18 @@ class UserController(
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('MANAGE_USERS')")
+    @Operation(summary = "Delete user",
+        description = "Delete user with a stated Id")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "User has been deleted", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "There is no user with such Id", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun deleteUser(@PathVariable("id") id: Long): BaseResponse {
         log.info("deleteUser(id = {})", id)
         userService.deleteUser(id)
@@ -89,6 +181,18 @@ class UserController(
     }
 
     @PostMapping("/createPasswordRestoreToken")
+    @Operation(summary = "Create password for current user",
+        description = "Check if user account exists and generate new random password to send to stated email address")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Password was successfully generated ", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "There is no user with such Email", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun createPasswordRestoreToken(@RequestParam email: String): BaseResponse {
         log.info("createPasswordRestoreToken(email = {})", email)
         userService.createPasswordRestoreToken(email)
@@ -96,6 +200,18 @@ class UserController(
     }
 
     @PostMapping("/restorePassword")
+    @Operation(summary = "Restore password for current user",
+        description = "Check if an old password exists and wasn't expired. If so replace an old password by a new one")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Password was successfully restored ", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "Your old password doesn't exist", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun restorePassword(@RequestParam token: String, @RequestParam newPassword: String): BaseResponse {
         log.info("restorePassword(token = {}, newPassword = ****)", token)
         userService.restorePassword(token, newPassword)
@@ -104,6 +220,16 @@ class UserController(
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/me")
+    @Operation(summary = "Get info about current user",
+        description = "Get info about current user")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Info about current user was successfully loaded", content = [
+            (Content(mediaType = "application/json", schema = Schema(implementation = UserDTO::class)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun getCurrentUser(): ObjectResponse<UserDTO> {
         log.info("getCurrentUser()")
         val user = securityService.getCurrentUser() ?: return ObjectResponse(ResponseCodes.INVALID_CREDENTIALS)
@@ -111,6 +237,18 @@ class UserController(
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get info about user with a stated Id",
+        description = "Get info about user with a stated Id if it exists")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Info about user was successfully loaded", content = [
+            (Content(mediaType = "application/json", schema = Schema(implementation = UserDTO::class)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "404", description = "User doesn't exist", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun getUser(
        @PathVariable("id") id: Long
     ): ObjectResponse<UserDTO> {
@@ -121,6 +259,16 @@ class UserController(
 
     @PreAuthorize("hasAuthority('MANAGE_USERS')")
     @GetMapping("/count")
+    @Operation(summary = "Get info quantity of registered users",
+        description = "Get info quantity of registered users")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Info about quantity of users was successfully loaded", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun countUsers(): NumberResponse {
         log.info("countUsers()")
         val count = userService.countUsers()
@@ -129,6 +277,17 @@ class UserController(
 
     @PreAuthorize("hasAuthority('MANAGE_USERS')")
     @GetMapping("/list")
+    @Operation(summary = "Get list of registered users",
+        description = "Get list of registered users")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Info about users was successfully loaded", content = [
+            (Content(mediaType = "application/json", array = (
+                    ArraySchema(schema = Schema(implementation = UserDTO::class)))))]),
+        ApiResponse(responseCode = "400", description = "Bad request", content = [
+            (Content(schema = Schema(hidden = true)))]),
+        ApiResponse(responseCode = "500", description = "Internal Server Error", content = [
+            (Content(schema = Schema(hidden = true)))])]
+    )
     fun listUsers(
             @RequestParam(required = false, defaultValue = "0") page: Int,
             @RequestParam(required = false, defaultValue = "10") size: Int
